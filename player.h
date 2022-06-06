da8d56dfb3341c43559913001eb542e478b0ffe6
LoveSnowEx <lsktw22@gmail.com>
2022-06-07 06:19:09 +0800

    feat(player.h): add free_player()

--- player.h
+++ player.h
@@ -18,3 +18,9 @@ sPlayer *new_player() {
 	sPlayer *pPlayer = calloc(1, sizeof(sPlayer));
 	return pPlayer;
 }
+
+void free_player(sPlayer *pPlayer) {
+	free_list(pPlayer->cards);
+	free_list(pPlayer->desk);
+	free(pPlayer);
+}
