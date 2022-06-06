e8b0ebd0ab69f5ff5f13ee78a2c2991713113742
LoveSnowEx <lsktw22@gmail.com>
2022-06-07 06:01:09 +0800

    add(role.h): create role.h

---
+++ role.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "basic.h"
+
+enum eRole {
+    Sheriff = 0,
+    Renegade,
+    Outlaws,
+    Deputy,
+};
+
+
+// usage: ROLE_DIVIDED[players][role]
+static const i32 ROLE_DIVIDED[8][4] = {
+    {0, 0, 0, 0},  // 0
+    {0, 0, 0, 0},  // 1
+    {0, 0, 0, 0},  // 2
+    {0, 0, 0, 0},  // 3
+    {1, 1, 2, 0},  // 4
+    {1, 1, 2, 1},  // 5
+    {1, 1, 3, 1},  // 6
+    {1, 1, 3, 2},  // 7 players
+};
