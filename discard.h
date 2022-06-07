bdb7f158b4ff88dc366614676074307e6044bd92
Yunyun0404 <v04040507@gmail.com>
2022-06-07 13:15:42 +0800

    undo(discard):discard.h

---
+++ discard.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "basic.h"
+#include "list.h"
+
+// 把 當前角色linked list 裡面的牌拿出來放到陣列裡面，讓玩家選擇要棄掉哪些牌。
+
+void discard( sGame *pGame ){
+    sListNode table[ size ];
+    i32 id=*(i32 *)pGame->cur_player->data;
+    i32 size=pGame->players[ id ]->cards.size;
+    sListNode pNode=pGame->player[id]->cards->end.next;
+
+    for( i32 i=0 ; i<size ; ++i ){
+        table[i]=pNode;
+        pNode=pNode.next;
+    }
+
+    for( i32 i=0 ; i<size ; ++i){
+        printf(" - %d : %s\n",i, cards[table[i].data].name );
+        printt(" -      %s\n",cards[table[i].data].discription);
+    }
+}
+
+void inquire( sListNode *table ){
+    printf("==========================================\n")
+    printf("Please enter the number of card to discard it: ");
+}
