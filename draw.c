b5dace261ef7c2915575304f15d9f36a9fd1066f
Beck049 <beckxiao2001323@gmail.com>
2022-06-11 04:09:00 +0800

    feat(draw.c): jesse(not yet) kit(done) pedro(done)

--- draw.c
+++ draw.c
@@ -58,13 +58,83 @@ void draw_phase_event_black_jack(sGame *pGame, sDrawPhaseEvent *e) {
 	
 void draw_phase_event_jesse_jones(sGame *pGame, sDrawPhaseEvent *e) {
 	
-	for(int i = 0; i < 2; ++i) {
+	for(i32 i = 0; i < 2; ++i) {
 		// draw 1 card from draw_pile
 		i32 card_id = take_card(pGame, pGame->draw_pile, 0);
 		if(card_id == -1) break;
 		give_card(pGame, pGame->players[e->target_id].cards, card_id, true);
 		if( i == 1 ) {
 			// select draw first card from draw_pile or player
+			sSelectEvent event = select_event(pGame, e->target_id, 1, 1, "1) draw from deck pile", "2) draw from player");
+			if( *(i32*)LIST_FRONT(event.select_res) == 1 )
+			{
+				card_id = take_card(pGame, pGame->draw_pile, 0);
+				if(card_id == -1) break;
+				give_card(pGame, pGame->players[e->target_id].cards, card_id, true);
+			}
+			else
+			{
+				// sSelectEvent choose player
+					// sSelectEvent choose card from player
+				// card_id = take_card(pGame, //sList *src// , 0);
+				if(card_id == -1) break;
+				give_card(pGame, pGame->players[e->target_id].cards, card_id, true);
+			}
+			// free
+			free(event.selections);
+			free(event.select_res);
+		}
+	}
+}
+	
+void draw_phase_event_kit_carlson(sGame *pGame, sDrawPhaseEvent *e) {
+	// draw 3 card from draw_pile
+	i32 card_id[3];
+	for(i32 i = 0; i < 3; ++i ) {
+		card_id[i] = take_card(pGame, pGame->draw_pile, 0);
+		if(card_id == -1) break;
+	}
+	// select two of them
+	char* str[3];
+	char *card_1 = "1) ", *card_2 = "2) ", *card_3 = "3) ";
+	str[0] = card_1; str[1] = card_2; str[2] = card_3;
+
+	for(int i = 0; i < 3; ++i) {
+		strcat(str[i], cards[card_id[0]].name); 
+		strcat(str[i], ":\n");
+		strcat(str[i], cards[card_id[0]].description);
+	}
+
+	sSelectEvent event = select_event(pGame, e->target_id, 2, 2, card_1, card_2, card_3);
+	// push the other one onto top of draw_pile
+	LIST_FOR_EACH(pNode, event.select_res) {
+		give_card(pGame, pGame->players[e->target_id].cards, card_id[(i32)pNode->data], true);
+		card_id[(i32)pNode->data] = -1;
+	}
+	LIST_FOR_EACH(pNode, event.select_res) {
+		if(card_id[(i32)pNode->data] != -1)
+		{
+			list_push_front( pGame->draw_pile, pNode );
+		}
+	}
+	// free
+	for(int i = 0; i < 3; ++i)
+	{
+		free(str[i]);
+	}
+	free(event.selections);
+	free(event.select_res);
+}
+	
+void draw_phase_event_pedro_ramirez(sGame *pGame, sDrawPhaseEvent *e) {
+	
+	for(i32 i = 0; i < 2; ++i) {
+		// draw 1 card from draw_pile
+		i32 card_id = take_card(pGame, pGame->draw_pile, 0);
+		if(card_id == -1) break;
+		give_card(pGame, pGame->players[e->target_id].cards, card_id, true);
+		if( i == 1 ) {
+			// select draw first card from draw_pile or discard_pile
 			sSelectEvent event = select_event(pGame, e->target_id, 1, 1, "1) draw from deck pile", "2) draw from discard pile");
 			if( *(i32*)LIST_FRONT(event.select_res) == 1 )
 			{
@@ -78,19 +148,11 @@ void draw_phase_event_jesse_jones(sGame *pGame, sDrawPhaseEvent *e) {
 				if(card_id == -1) break;
 				give_card(pGame, pGame->players[e->target_id].cards, card_id, true);
 			}
+			// free
+			free(event.selections);
+			free(event.select_res);
 		}
 	}
-}
-	
-void draw_phase_event_kit_carlson(sDrawPhaseEvent *e) {
-	// draw 3 card from draw_pile
-	// select two of them
-	// push the other one onto top of draw_pile
-}
-	
-void draw_phase_event_pedro_ramirez(sDrawPhaseEvent *e) {
-	// select draw first card from draw_pile or discard_pile
-	// draw 1 card from draw_pile
 }
 
 // player get N cards form draw pile
