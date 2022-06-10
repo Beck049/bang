14f9ff34064ce0fc6e161a07f3d4acf689e4ce27
LoveSnowEx <lsktw22@gmail.com>
2022-06-11 00:49:16 +0800

    add(select.h): create select.h and select.c

---
+++ select.c
@@ -0,0 +1,9 @@
+#include "select.h"
+
+void select_event_player(sSelectEvent *e) {
+
+}
+
+void select_event_bot(sSelectEvent *e) {
+	
+}
---
+++ select.h
@@ -0,0 +1,7 @@
+#pragma once;
+
+#include "event.h"
+
+void select_event_player(sSelectEvent *e);
+
+void select_event_bot(sSelectEvent *e);
