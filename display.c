49bca336cfaa056049c67cf07cfb023de2f74c30
LoveSnowEx <lsktw22@gmail.com>
2022-06-16 21:54:34 +0800

    feat(display.h): create display_game()

--- display.c
+++ display.c
@@ -10,3 +10,33 @@ void display_pile(sList *pList) {
 		++row;
 	}
 }
+
+void display_game(sGame *pGame, i32 target_id) {
+	printf( "-----------------BANG-----------------\n"
+ 			" Draw Pile (%ld)   /  Discard Pile (%ld)\n"
+			"--------------------------------------\n", pGame->draw_pile->size, pGame->discard_pile->size);
+	sList *live_player = pGame->live_players;
+
+	sListNode *player_node = get_player(pGame, target_id);
+	if(player_node == NULL) {
+		player_node = LIST_FRONT(live_player);
+	}
+
+	i32 size = live_player->size;
+	for(i32 i = 0; i < size; ++i) {
+		i32 player_id = *(i32*)player_node->data;
+		sList *player_cards = pGame->players[player_id].cards;
+		sList *player_desk = pGame->players[player_id].desk;
+		char appellation[16];
+		if(player_id == target_id) {
+			sprintf(appellation, "Your");
+		}
+		else {
+			sprintf(appellation, "Player%d's", player_id);
+		}
+		printf("%s Cards (%ld):\n", appellation, player_cards->size);
+		display_pile(player_cards);
+		printf("%s Desk (%ld):\n", appellation, player_desk->size);
+		display_pile(player_desk);
+	}
+}
--- display.h
+++ display.h
@@ -1,5 +1,8 @@
 #include "basic.h"
 #include "list.h"
 #include "init_card.h"
+#include "game_play.h"
 
 void display_pile(sList *pList);
+
+void display_game(sGame *pGame, i32 target_id);
