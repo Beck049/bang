3b32e57bf80604d92363f25b375d0900a365ca79
LoveSnowEx <lsktw22@gmail.com>
2022-06-19 05:30:55 +0800

    refact(card_func): move declaration of card_funcs to top

--- card_func.c
+++ card_func.c
@@ -1,5 +1,6 @@
 #include "card_func.h"
 
+void (*card_funcs[80])(sGame *pGame, i32 player_id);
 
 void card_miss(sGame *pGame, i32 player_id) {
 
@@ -121,8 +122,3 @@ void card_cat_balou(sGame *pGame, i32 player_id) {
 
 	printf("player %d 棄掉了 %s\n", target_id, cards[ card_id ].name);
 }
-
-void (*card_funcs[80])(sGame *pGame, i32 player_id) = {
-	&card_miss,
-	&card_bang,
-};
--- card_func.h
+++ card_func.h
@@ -4,6 +4,8 @@
 #include "draw.h"
 #include "event.h"
 
+extern void (*card_funcs[80])(sGame *pGame, i32 player_id);
+
 // cards
 // void card_miss(sGame *pGame, i32 player_id);
 // void card_bang(sGame *pGame, i32 player_id);
@@ -33,6 +35,3 @@ void card_cat_balou(sGame *pGame, i32 player_id);
 // void winchester(sGame *pGame, i32 player_id);
 // void carabine(sGame *pGame, i32 player_id);
 // void volcano(sGame *pGame, i32 player_id);
-
-
-extern void (*card_funcs[80])(sGame *pGame, i32 player_id);
