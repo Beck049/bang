19769481b2e9168bba065d6bd6647cdb67a27a2c
Beck049 <beckxiao2001323@gmail.com>
2022-06-18 16:42:26 +0800

    fix(dodge_event) add dodge_times to struct

--- dodge.c
+++ dodge.c
@@ -5,7 +5,7 @@ bool card_is_missed(i32 card_id) {
 	return cards[card_id].type == MISS_TYPE;
 }
 
-void dodge_event_default(sGame *pGame, sDodgeEvent *e) {
+void dodge_event_default(sGame *pGame, sDodgeEvent *e, int dodge_times) {
 	sList *player_cards = pGame->players[e->target_id].cards;
 	sList *miss_cards = card_filter(player_cards, card_is_missed);
 	if(miss_cards->size == 0) {
@@ -26,7 +26,7 @@ void dodge_event_default(sGame *pGame, sDodgeEvent *e) {
 			i32 card_id = *(i32*)cur_node->data;
 			sprintf(options[i], "%2d) %s (id: %d):\n%s", i, cards[card_id].name, card_id, cards[card_id].description);
 		}
-		sSelectEvent select_miss_e = select_event_with_arr(pGame, e->target_id, 1, 1, options, miss_cards->size, 128);
+		sSelectEvent select_miss_e = select_event_with_arr(pGame, e->target_id, dodge_times, dodge_times, options, miss_cards->size, 128);
 		i32 select_card_id = miss_card_id[*(i32*)LIST_FRONT(select_miss_e.select_res)];
 		i32 take_id = take_card_by_id(pGame, player_cards, select_card_id);
 		if(take_id != -1) {
