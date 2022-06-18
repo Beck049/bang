a88958061fb592c4dff9bca53abb13a8d2fc7c58
Beck049 <beckxiao2001323@gmail.com>
2022-06-18 23:37:39 +0800

    add(lethal.c): add file lethal.c .h

---
+++ lethal.c
@@ -0,0 +1,5 @@
+#include"lethal.h"
+
+void lethal_event_default(sGame *pGame, sLethalEvent *e);
+
+void lethal_event_sid_ketchum(sGame *pGame, sLethalEvent *e);
---
+++ lethal.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "event.h"
+#include "game_play.h"
+#include "list.h"
+
+void lethal_event_default(sGame *pGame, sLethalEvent *e);
+
+void lethal_event_sid_ketchum(sGame *pGame, sLethalEvent *e);
