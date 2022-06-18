c88b4c69f88200dab5354eb5585ac1df9213f6d3
Beck049 <beckxiao2001323@gmail.com>
2022-06-19 06:33:11 +0800

    fix(damage.c): variables

--- damage.c
+++ damage.c
@@ -1,28 +1,26 @@
 #include "damage.h"
 
 void damage_event_default(sGame *pGame, sDamageEvent *e) {
-    sListNode *cur_p = get_player(pGame, e->victim_id);
-    pGame->players[*(i32*)cur_p->data].hp -= e->damage;
+    pGame->players[e->victim_id].hp -= e->damage;
 }
 
 // draw
 void damage_event_bart_cassidy(sGame *pGame, sDamageEvent *e) {
-    sListNode *cur_p = get_player(pGame, e->victim_id);
-    pGame->players[*(i32*)cur_p->data].hp -= e->damage;
+    pGame->players[e->victim_id].hp -= e->damage;
 
     for(i32 i = 0; i < e->damage; ++i) {
         i32 card_id = take_card(pGame, pGame->draw_pile, 0);
 		if(card_id == -1) break;
-		give_card(pGame, pGame->players[*(i32*)cur_p->data].cards, card_id, true);
+		give_card(pGame, pGame->players[e->victim_id].cards, card_id, true);
     }
 }
 
 void damage_event_el_gringo(sGame *pGame, sDamageEvent *e) {
-    sListNode *cur_p = get_player(pGame, e->victim_id);
-    pGame->players[*(i32*)cur_p->data].hp -= e->damage;
+    pGame->players[e->victim_id].hp -= e->damage;
 
+    sList *card_list = pGame->players[e->damager_id].cards;
     for(i32 i = 0; i < e->damage; ++i) {
-        i32 hand_card_num  = pGame->players[e->damager_id].cards->size;
+        i32 hand_card_num  = card_list->size;
         char cards_opt[hand_card_num][8];
         for(int i = 0; i < hand_card_num; ++i ) {
 
@@ -32,7 +30,7 @@ void damage_event_el_gringo(sGame *pGame, sDamageEvent *e) {
         sSelectEvent card_select_event = select_event_with_arr(pGame, e->damager_id, 1, 1, cards_opt, hand_card_num, 8);
     
         i32 take_id = *(i32*)LIST_FRONT(card_select_event.select_res);
-        card_id = take_card(pGame, pGame->players[e->damager_id].cards, take_id);
+        card_id = take_card(pGame, card_list, take_id);
         if(card_id != -1) {
             give_card(pGame, pGame->players[e->victim_id].cards, card_id, true);
         }
