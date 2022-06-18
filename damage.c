e5372da81beea1ce8abe8e19c23f494a7b00daad
Beck049 <beckxiao2001323@gmail.com>
2022-06-18 22:57:41 +0800

    add(damage.c): add damage.c damage.h

---
+++ damage.c
@@ -0,0 +1,7 @@
+#include "damage.h"
+
+void damage_event_default(sGame *pGame, sDetermineEvent *e);
+
+void damage_event_bart_cassidy(sGame *pGame, sDetermineEvent *e);
+
+void damage_event_el_gringo(sGame *pGame, sDetermineEvent *e);
---
+++ damage.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "event.h"
+#include "game_play.h"
+
+void damage_event_default(sGame *pGame, sDetermineEvent *e);
+
+void damage_event_bart_cassidy(sGame *pGame, sDetermineEvent *e);
+
+void damage_event_el_gringo(sGame *pGame, sDetermineEvent *e);
