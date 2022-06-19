66d4378b5e36494cad3270769dc1ec652591610a
LoveSnowEx <lsktw22@gmail.com>
2022-06-19 19:54:34 +0800

    fix(draw.h): declare functions in draw.h

--- draw.h
+++ draw.h
@@ -12,3 +12,12 @@ i32 take_card_by_id(sGame *pGame, sList *src, i32 card_id);
 void give_card(sGame *pGame, sList *dst, i32 card_id, bool tail_insert);
 
 void draw_phase_event_default(sGame *pGame, sDrawPhaseEvent *e);
+
+
+void draw_phase_event_black_jack(sGame *pGame, sDrawPhaseEvent *e);
+	
+void draw_phase_event_jesse_jones(sGame *pGame, sDrawPhaseEvent *e);
+	
+void draw_phase_event_kit_carlson(sGame *pGame, sDrawPhaseEvent *e);
+	
+void draw_phase_event_pedro_ramirez(sGame *pGame, sDrawPhaseEvent *e);
