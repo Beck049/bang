453a0318073fb2fc0bce47cd3c25ebbe6ed4432c
Yunyun0404 <v04040507@gmail.com>
2022-06-20 07:40:02 +0800

    fix(determine.c)
    \n

--- determine.c
+++ determine.c
@@ -5,7 +5,7 @@ void determine_event_default(sGame *pGame, sDetermineEvent *e) {
 	display_determine(pGame, e->target_id, 1);
 	i32 card_id = take_card(pGame, pGame->draw_pile, 0);
 	print_card(buf, card_id);
-	printf(" 1) %s", buf);
+	printf(" 1) %s\n", buf);
 	give_card(pGame, pGame->discard_pile, card_id, false);
 	e->determine_res = card_id;
 }
