91c19a9201e03ea39de4434a53a4a9545b5675e1
LoveSnowEx <lsktw22@gmail.com>
2022-06-20 06:38:15 +0800

    feat(damage.c): show damage info

--- damage.c
+++ damage.c
@@ -2,12 +2,14 @@
 
 void damage_event_default(sGame *pGame, sDamageEvent *e) {
     pGame->players[e->victim_id].hp -= e->damage;
+    printf("Player%d 受到 %d 點傷害，血量剩餘: %d\n", e->victim_id, pGame->players[e->victim_id].hp);
 }
 
 // draw
 void damage_event_bart_cassidy(sGame *pGame, sDamageEvent *e) {
-    pGame->players[e->victim_id].hp -= e->damage;
+    damage_event_default(pGame, e);
 
+    printf("Player%d 可以從排堆抽出 %d 張牌加到自己手上\n", e->victim_id, e->damage);
     for(i32 i = 0; i < e->damage; ++i) {
         i32 card_id = take_card(pGame, pGame->draw_pile, 0);
 		if(card_id == -1) break;
@@ -16,15 +18,15 @@ void damage_event_bart_cassidy(sGame *pGame, sDamageEvent *e) {
 }
 
 void damage_event_el_gringo(sGame *pGame, sDamageEvent *e) {
-    pGame->players[e->victim_id].hp -= e->damage;
+    damage_event_default(pGame, e);
 
+    printf("Player%d 可以從 player %d 手上抽出 %d 張牌加到自己手上\n", e->victim_id, e->damager_id, e->damage);
     sList *card_list = pGame->players[e->damager_id].cards;
     for(i32 i = 0; i < e->damage; ++i) {
         i32 hand_card_num  = card_list->size;
         char cards_opt[hand_card_num][512];
         for(int i = 0; i < hand_card_num; ++i ) {
-
-            sprintf(cards_opt[i], "(%2d)", i);
+            sprintf(cards_opt[i], "%2d) player%d 的手排%d", i+1, i+1);
         }
         i32 card_id;
         sSelectEvent card_select_event = select_event_with_arr(pGame, e->damager_id, 1, 1, cards_opt, hand_card_num, sizeof(*cards_opt));
