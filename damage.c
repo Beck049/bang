bc641fdd7d6bd813fd6b034be0d95d99401df957
LoveSnowEx <lsktw22@gmail.com>
2022-06-20 16:35:02 +0800

    feat(damage.c): display chinese in damage.c

--- damage.c
+++ damage.c
@@ -2,14 +2,22 @@
 
 void damage_event_default(sGame *pGame, sDamageEvent *e) {
     pGame->players[e->victim_id].hp -= e->damage;
-    printf("Player%d 受到 %d 點傷害，血量剩餘: %d\n", e->victim_id, e->damage, pGame->players[e->victim_id].hp);
+    if(e->victim_id == 0) {
+        printf("你 受到 %d 點傷害，血量剩餘: %d\n", e->damage, pGame->players[e->victim_id].hp);
+    } else {
+        printf("玩家 %d 受到 %d 點傷害，血量剩餘: %d\n", e->victim_id, e->damage, pGame->players[e->victim_id].hp);
+    }
 }
 
 // draw
 void damage_event_bart_cassidy(sGame *pGame, sDamageEvent *e) {
     damage_event_default(pGame, e);
 
-    printf("Player%d 可以從排堆抽出 %d 張牌加到自己手上\n", e->victim_id, e->damage);
+    if(e->victim_id == 0) {
+        printf("[能力] 你 從排堆抽出 %d 張牌加到自己手上\n", e->damage);
+    } else {
+        printf("[能力] 玩家 %d 從排堆抽出 %d 張牌加到自己手上\n", e->victim_id, e->damage);
+    }
     for(i32 i = 0; i < e->damage; ++i) {
         i32 card_id = take_card(pGame, pGame->draw_pile, 0);
 		if(card_id == -1) break;
@@ -19,16 +27,31 @@ void damage_event_bart_cassidy(sGame *pGame, sDamageEvent *e) {
 
 void damage_event_el_gringo(sGame *pGame, sDamageEvent *e) {
     damage_event_default(pGame, e);
+    char victim_appellation[16], damager_appellation[16];
 
     if(e->damager_id == -1) return;
 
-    printf("Player%d 可以從 player %d 手上抽出 %d 張牌加到自己手上\n", e->victim_id, e->damager_id, e->damage);
+    if(e->victim_id == 0) {
+		sprintf(victim_appellation, "你");
+	}
+	else {
+		sprintf(victim_appellation, "玩家 %d", e->victim_id);
+	}
+	if(e->damager_id == 0) {
+		sprintf(damager_appellation, "你");
+	}
+	else {
+		sprintf(damager_appellation, "玩家 %d", e->damager_id);
+	}
+
+    printf("[能力] %s 可以從 %s 手上抽出 %d 張牌加到自己手上\n", victim_appellation, damager_appellation, e->damage);
+
     sList *card_list = pGame->players[e->damager_id].cards;
     for(i32 i = 0; i < e->damage; ++i) {
         i32 hand_card_num  = card_list->size;
         char cards_opt[hand_card_num][512];
         for(int i = 0; i < hand_card_num; ++i ) {
-            sprintf(cards_opt[i], "%2d) player%d 的手牌%d", i+1, e->damager_id, i+1);
+            sprintf(cards_opt[i], "%2d) 手牌 %d", i+1, i+1);
         }
         i32 card_id;
         sSelectEvent card_select_event = select_event_with_arr(pGame, e->victim_id, 1, 1, cards_opt, hand_card_num, sizeof(*cards_opt));
